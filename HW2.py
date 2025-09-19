896ecdcbcba74c5e51edaf24373b30c7335c5aae
Deven Abisha 林順義 <menolakempatget@gmail.com>
2025-09-19 22:03:00 +0800

    Update HW2.py

--- HW2.py
+++ HW2.py
@@ -7,20 +7,15 @@ c = 3
 def root(a,b,c):
     judge = b ** 2 - 4 * a * c 
 
-    if judge > 0:
+    if judge != 0:
         r1 = (-b + cmath.sqrt(judge)) / (2*a)
         r2 = (-b - cmath.sqrt(judge)) / (2*a)
-        return r1, r2
     elif judge == 0:
         r1 = -b / (2*a)
-        r2 == r1
-        return r1, r2
-    else :
-        real = -b / 2*a
-        imag = cmath.sqrt(judge) / (2*a)
-        r1 = f"{real} + {imag}"
-        r2 = f"{real} - {imag}"
-    print("Root 1 = ", r1)
-    print("Root 2 = ", r2)
+        r2 = r1
+    return r1, r2
+        
+r1, r2 = root(a,b,c)
+print("Root 1 = ", r1)
+print("Root 2 = ", r2)
 
-root(a,b,c)
